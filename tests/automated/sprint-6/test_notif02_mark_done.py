d42255ab77f483d55027489373b20a3899b52385
Allan John Funelas <funelasallanjohn@gmail.com>
2026-05-02 06:15:21 +0800

    test(sprint-6): created yaml file and updated seed.sql

--- tests/automated/sprint-6/test_notif02_mark_done.py
+++ tests/automated/sprint-6/test_notif02_mark_done.py
@@ -34,7 +34,7 @@ CARD_SELECTOR = (
 
 def make_driver():
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-blink-features=AutomationControlled")
